d6cdc8a0a90037729985b093a6e592870b7376a2
Lucas Gigliozzi <lucasg@esna.com>
2019-11-07 17:14:26 -0500

    added comments and removed some files

---
+++ EmguCVTest2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace EmguCVTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(611, 380);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(650, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(611, 380);
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 549);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Mask Original";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Color Analysis";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EmguCVTest2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+
+namespace EmguCVTest2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Image<Bgr, byte> imgInput = new Image<Bgr, byte>("d:\\Visual Studio Projects\\droneviewoffarmland.jpg");
+
+        Image<Gray, byte> gray_image; //for gray scale images
+
+        Bgr[] imgBgr = new Bgr[2073600];
+
+
+        Bgr compare_color_1 = new Bgr(10, 223, 220); //Brown shade 1 -- there will be over 300 here eventually to compare with
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            imageBox1.Image = imgInput;
+
+            //imageBox2.Image = imgInput.InRange(new Bgr(10, 0, 0),  // min filter value ( if color is greater than or equal to this)
+             //                                    new Bgr(20, 255, 255)); // max filter value ( if color is less than or equal to this)
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0, j = 0;
+
+            //gray_image = imgInput.Convert<Gray, byte>();
+
+            byte[,,] pixeldata = imgInput.Data;      //use this inside of for loops and while loops since it is much faster in C#, rather than using imgInput.Data on its own
+
+
+            //pixeldata[i, j, 0] = 0; //blue
+            // pixeldata[i, j, 1] = 0; //green 
+            // pixeldata[i, j, 2] = 0; //red
+
+            imgBgr[0] = imgInput[0, 0];
+
+            //imgBgr[0] = imgInput[0, 0]; grab pixels directly
+            // Color pixel_color = Color.FromArgb(imgInput.Data[0, 0, 0], set to color variable instead
+            // imgInput.Data[0, 0, 1], imgInput.Data[0, 0, 2]);
+
+            Image<Bgr, byte> resizedimg = imgInput.Resize(1280, 720, Emgu.CV.CvEnum.Inter.Cubic); //resizing image cubicly down to 1280 x 720 pixels
+
+            imageBox1.Image = resizedimg;
+
+            for (i = 0; i < 550; i++) //loop by pixel size of picture, xsize 
+            {
+                for (j = 0; j < 1000; j++)//loop by pixel size of picture, ysize 
+                { //if all three bgr values of the current pixel and the predetermined color match, then it is true
+                    if ((pixeldata[i, j, 0] == compare_color_1.Blue) && (pixeldata[i, j, 1] == compare_color_1.Green) && (pixeldata[i, j, 2] == compare_color_1.Red)) 
+                    {
+                        imgInput[i, j] = new Bgr(0,0,255); //changes the current pixel to red if true
+
+                    }
+
+                }
+
+                
+
+            }
+         imageBox1.Image = imgInput; //display the new image to output
+
+        }
+    }
+}
